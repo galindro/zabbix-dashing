2138cb80af6af52e651878262391dc1920c1fa3f
root <Bruno Galindro da Costa>
2015-11-03 22:11:00 +0000

    Fix crash when a template isn't associated with any host(s)

--- jobs/zabbix.rb
+++ jobs/zabbix.rb
@@ -3,203 +3,212 @@ require "json"
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m', :first_in => 0 do |job|
-	
-	config_file = File.read(Dir.pwd + '/jobs/zabbix-monitor-config.json')
-	config_parsed = JSON.parse(config_file)
+  
+  config_file = File.read(Dir.pwd + '/jobs/zabbix-monitor-config.json')
+  config_parsed = JSON.parse(config_file)
 
-	#Your template names
-	templates = config_parsed['Zabbix']['templates']
+  #Your template names
+  templates = config_parsed['Zabbix']['templates']
 
-	#Your auth token
-	auth_token = config_parsed['auth_token']
+  #Your auth token
+  auth_token = config_parsed['auth_token']
 
-	#Your Zabbix API configuration
-	zbx = ZabbixApi.connect(
-		:url => config_parsed['Zabbix']['api_config']['url'],
-		:user => config_parsed['Zabbix']['api_config']['user'],
-		:password => config_parsed['Zabbix']['api_config']['password']
-	)
+  #Your Zabbix API configuration
+  zbx = ZabbixApi.connect(
+    :url => config_parsed['Zabbix']['api_config']['url'],
+    :user => config_parsed['Zabbix']['api_config']['user'],
+    :password => config_parsed['Zabbix']['api_config']['password']
+  )
 
-	zbx_priorities = {
-		0 => "ok",
-		1 => "nothing",
-		2 => "warning",
-		3 => "average",
-		4 => "high",
-		5 => "disaster",
-		10 => "acknowledged"
-	}
+  zbx_priorities = {
+    0 => "ok",
+    1 => "nothing",
+    2 => "warning",
+    3 => "average",
+    4 => "high",
+    5 => "disaster",
+    10 => "acknowledged"
+  }
 
-	templateids = []
-	application_names = []
+  templateids = []
+  application_names = []
 
-	#Get template ids
-	template_ids = zbx.query(
-		:method => "template.get",
-		:params => {
-			:filter => {
-				:host => templates
-			},
-			:output => [
-				"templateid"
-			]
-		}
-	)
+  #Get template ids
+  template_ids = zbx.query(
+    :method => "template.get",
+    :params => {
+      :filter => {
+        :host => templates
+      },
+      :output => [
+        "templateid"
+      ]
+    }
+  )
 
-	#Gen list of template ids
-	(template_ids).each do |template|
-		templateids << template['templateid']
-	end
+  #Gen list of template ids
+  (template_ids).each do |template|
+    templateids << template['templateid']
+  end
 
-	if not config_parsed['Zabbix'].has_key?('applications_include')
-		#Get application names from template ids
-		applications = zbx.query(
-			:method => "application.get",
-			:params => {
-				:templated => true,
-				:templateids => templateids,
-				:output => [
-					"name"
-				]
-			}
-		)
-	else
-		#Get application names from template ids
-		applications = zbx.query(
-			:method => "application.get",
-			:params => {
-				:templated => true,
-				:templateids => templateids,
-				:filter => {
-					:name => config_parsed['Zabbix']['applications_include'],
-				},
-				:output => [
-					"name"
-				]
-			}
-		)
-	end
+  if not config_parsed['Zabbix'].has_key?('applications_include')
+    #Get application names from template ids
+    applications = zbx.query(
+      :method => "application.get",
+      :params => {
+        :templated => true,
+        :templateids => templateids,
+        :output => [
+          "name"
+        ]
+      }
+    )
+  else
+    #Get application names from template ids
+    applications = zbx.query(
+      :method => "application.get",
+      :params => {
+        :templated => true,
+        :templateids => templateids,
+        :filter => {
+          :name => config_parsed['Zabbix']['applications_include'],
+        },
+        :output => [
+          "name"
+        ]
+      }
+    )
+  end
 
-	#Gen list of application names
-	(applications).each do |application|
-		if config_parsed['Zabbix'].has_key?('applications_exclude')
-			if config_parsed['Zabbix']['applications_exclude'].include?(application['name'])
-				next
-			end
-		end
-		application_names << application['name']
-	end
+  #Gen list of application names
+  (applications).each do |application|
+    if config_parsed['Zabbix'].has_key?('applications_exclude')
+      if config_parsed['Zabbix']['applications_exclude'].include?(application['name'])
+        next
+      end
+    end
+    application_names << application['name']
+  end
 
-	application_names = application_names.uniq
+  if application_names.any?
 
-	(application_names).each do |app_name|
+    application_names = application_names.uniq
 
-		triggers = []
-		items = []
-		events = []
-		last_priority = 0
-		priority = 0
-		trigger_name = ""
-		trigger_id = ""
-		acks = ""
+    (application_names).each do |app_name|
 
-		#Get application ids inherited from templates that is equal to app_name
-		application = zbx.query(
-			:method => "application.get",
-			:params => {
-				:inherited => true,
-				:filter => {
-					:name => app_name,
-				},
-				:output => [
-					"applicationid"
-				]
-			}
-		)
+      triggers = []
+      items = []
+      events = []
+      last_priority = 0
+      priority = 0
+      trigger_name = ""
+      trigger_id = ""
+      acks = ""
 
-		#Get triggers with state problem where application ids is equal to application[0]['applicationid']
-		triggers = zbx.query(
-			:method => "trigger.get",
-			:params => {
-				:filter => {
-					:value => 1,
-					:status => 0
-				},
-				:applicationids => [application[0]['applicationid']],
-				:output => [
-					"description",
-					"priority",
-					"triggerid"
-				]
-			}
-		)
+      #Get application ids inherited from templates that is equal to app_name
+      application = zbx.query(
+        :method => "application.get",
+        :params => {
+          :inherited => true,
+          :filter => {
+            :name => app_name,
+          },
+          :output => [
+            "applicationid"
+          ]
+        }
+      )
 
-		#If there are triggers
-		if triggers.any?
-		
-			(triggers).each do |trigger|
+      if application.any?
 
-				#Get the greater priority
-				if trigger['priority'].to_i > last_priority
-					priority = trigger['priority'].to_i
-					last_priority = priority
-					trigger_name = trigger['description']
-					trigger_id = trigger['triggerid']
-				end
+        #Get triggers with state problem where application ids is equal to application[0]['applicationid']
+        triggers = zbx.query(
+          :method => "trigger.get",
+          :params => {
+            :filter => {
+              :value => 1,
+              :status => 0
+            },
+            :applicationids => [application[0]['applicationid']],
+            :output => [
+              "description",
+              "priority",
+              "triggerid"
+            ]
+          }
+        )
 
-			end
+        #If there are triggers
+        if triggers.any?
+        
+          (triggers).each do |trigger|
 
-			# Check associated items
-			items = zbx.query(
-				:method => "item.get",
-				:params => {
-					:triggerids => trigger_id,
-					:webitems => true,
-					:filter => {
-						:status => 0
-					},
-					:output => [
-						"itemid"
-					]
-				}
-			)
+            #Get the greater priority
+            if trigger['priority'].to_i > last_priority
+              priority = trigger['priority'].to_i
+              last_priority = priority
+              trigger_name = trigger['description']
+              trigger_id = trigger['triggerid']
+            end
 
-			# Check if the associated items are disabled
-			if not items.any?
-				priority = 0
-			else
-				#Get events from trigger with acknowledgedment = yes
-				events = zbx.query(
-					:method => "event.get",
-					:params => {
-						:objectids => trigger_id,
-						:select_acknowledges => "extend",
-						:sortfield => "clock",
-						:sortorder => "DESC",
-						:limit => 1,
-						:filter => {
-							:acknowledged => 1
-						},
-						:output => [
-							"eventid"
-						]
-					}
-				)
-			end
-		end
+          end
 
-		#If there are acknowledgedment
-		if events.any?
-			#Send event to the application widget with ack
-			send_event(app_name, { auth_token: auth_token, status: zbx_priorities[10], text: trigger_name })
-		else
-			#Send event to the application widget without ack
-			send_event(app_name, { auth_token: auth_token, status: zbx_priorities[priority], text: trigger_name })
-		end
+          # Check associated items
+          items = zbx.query(
+            :method => "item.get",
+            :params => {
+              :triggerids => trigger_id,
+              :webitems => true,
+              :filter => {
+                :status => 0
+              },
+              :output => [
+                "itemid"
+              ]
+            }
+          )
 
-	end
+          # Check if the associated items are disabled
+          if not items.any?
+            priority = 0
+          else
+            #Get events from trigger with acknowledgedment = yes
+            events = zbx.query(
+              :method => "event.get",
+              :params => {
+                :objectids => trigger_id,
+                :select_acknowledges => "extend",
+                :sortfield => "clock",
+                :sortorder => "DESC",
+                :limit => 1,
+                :filter => {
+                  :acknowledged => 1
+                },
+                :output => [
+                  "eventid"
+                ]
+              }
+            )
+          end
+        end
 
-	#Set the array zabbix_widget_names with the application_names that will be used for generate widgets
-	set :zabbix_widget_names, application_names
+        #If there are acknowledgedment
+        if events.any?
+          #Send event to the application widget with ack
+          send_event(app_name, { auth_token: auth_token, status: zbx_priorities[10], text: trigger_name })
+        else
+          #Send event to the application widget without ack
+          send_event(app_name, { auth_token: auth_token, status: zbx_priorities[priority], text: trigger_name })
+        end
+
+      end
+
+    end
+  
+  end
+
+  #Set the array zabbix_widget_names with the application_names that will be used for generate widgets
+  set :zabbix_widget_names, application_names
 
 end
+
